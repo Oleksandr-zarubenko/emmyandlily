9f86606c6302b71df3365560d034b6d189e3bceb
smerch88 <maxarmyk@gmail.com>
2024-01-28 20:03:54 +0200

    fix: typo

--- my-project/src/app/layout.tsx
+++ my-project/src/app/layout.tsx
@@ -10,9 +10,9 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "Emmy amd Lili - dog`s shampoo brand.",
+  title: "Emmy and Lili - dog`s shampoo brand.",
   description:
-    "Emmy amd Lili - dog`s shampoo brand. We need to be healthy and beautiful, so we have been looking for the best hair products for a long time to be shiny, smooth, and well-combed. However, we have never found a one-size-fits-all solution that meets our needs. That's how demanding we are! Then, we had the idea to invent our super formula for hair health. Our friends helped us a little, but they wouldn't have managed without us! So we invite you to the world of beauty! Try our formula, and let us know if you like it!",
+    "Emmy and Lili - dog`s shampoo brand. We need to be healthy and beautiful, so we have been looking for the best hair products for a long time to be shiny, smooth, and well-combed. However, we have never found a one-size-fits-all solution that meets our needs. That's how demanding we are! Then, we had the idea to invent our super formula for hair health. Our friends helped us a little, but they wouldn't have managed without us! So we invite you to the world of beauty! Try our formula, and let us know if you like it!",
 };
 
 export default function RootLayout({
