c0ef55e7a8a75e6a9fac2a5ba66651030bf41b17
smerch88 <maxarmyk@gmail.com>
2024-02-06 22:27:01 +0200

    fix

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -64,7 +64,7 @@ export async function generateMetadata(): Promise<Metadata> {
       title: "Emmy and Lili - dog`s shampoo brand.",
       description:
         "Emmy and Lili - dog`s shampoo brand. We need to be healthy and beautiful, so we have been looking for the best hair products for a long time to be shiny, smooth, and well-combed. However, we have never found a one-size-fits-all solution that meets our needs. That's how demanding we are! Then, we had the idea to invent our super formula for hair health. Our friends helped us a little, but they wouldn't have managed without us! So we invite you to the world of beauty! Try our formula, and let us know if you like it!",
-      url: process.env.HOST,
+      url: "https://www.emmyandlily.com/",
       siteName: "Emmy and Lili - dog`s shampoo brand.",
       images: [
         {
