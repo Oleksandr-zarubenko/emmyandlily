a7487c71c45fd62d97587b4eebff8a1342d606e3
smerch88 <maxarmyk@gmail.com>
2024-02-06 22:14:53 +0200

    feat: add metadata

--- my-project/src/app/layout.tsx
+++ my-project/src/app/layout.tsx
@@ -15,6 +15,25 @@ export const metadata: Metadata = {
   title: "Emmy and Lili - dog`s shampoo brand.",
   description:
     "Emmy and Lili - dog`s shampoo brand. We need to be healthy and beautiful, so we have been looking for the best hair products for a long time to be shiny, smooth, and well-combed. However, we have never found a one-size-fits-all solution that meets our needs. That's how demanding we are! Then, we had the idea to invent our super formula for hair health. Our friends helped us a little, but they wouldn't have managed without us! So we invite you to the world of beauty! Try our formula, and let us know if you like it!",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
 };
 
 export default function RootLayout({
