943dc4b6c72cfc60dc61515d4c2be69ee7367814
smerch88 <maxarmyk@gmail.com>
2024-02-26 21:04:35 +0200

    fix ml hero dogs overflow

--- my-project/src/page-components/HeroSection.tsx
+++ my-project/src/page-components/HeroSection.tsx
@@ -5,7 +5,7 @@ export const HeroSection = ({ data }: { data: any }) => {
   return (
     <div className="relative bg-bg_primary text-center xl:text-left">
       <div className="hero container flex items-center xl:h-[800px]">
-        <div className="mx-auto mb-96 mt-32 flex flex-col md:mb-[642px] md:max-w-[416px] xl:mb-0 xl:ml-5 xl:mt-0 xl:max-w-96 smOnly:p-2">
+        <div className="mx-auto mb-96 mt-32 flex flex-col md:mb-[642px] md:max-w-[416px] xl:mb-0 xl:ml-0 xl:mt-0 xl:max-w-96 smOnly:p-2">
           <Markdown text={data.mainSection.heading} className="order-2" />
           <div className="order-1 mb-8 flex flex-row justify-center gap-1 text-dark xl:justify-start">
             <Paw className="h-12 w-12 flex-shrink-0 md:h-20 md:w-20" />
