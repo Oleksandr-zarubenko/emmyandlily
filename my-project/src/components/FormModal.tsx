f2e310448a402fd3e4ae39916b8a8f495c06ea27
smerch88 <maxarmyk@gmail.com>
2024-02-28 00:21:22 +0200

    fix

--- my-project/src/components/FormModal.tsx
+++ my-project/src/components/FormModal.tsx
@@ -98,9 +98,7 @@ export const FormModal = ({ orderForm }: { orderForm: any }) => {
         ...data,
         createdAt: new Date().toLocaleString(),
       };
-      const googleTableResult = await sendGoogleTable(newData);
-      console.log("_________");
-      console.log("Google Table Result:", googleTableResult);
+
       await Promise.all([
         sendGoogleTable(newData),
         sendUserToTelegramBot(newData),
