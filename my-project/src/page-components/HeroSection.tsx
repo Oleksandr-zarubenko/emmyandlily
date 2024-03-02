b318a537128128584dc35aff889f5574281db7b1
smerch88 <maxarmyk@gmail.com>
2024-03-02 12:00:33 +0200

    colors

--- my-project/src/page-components/HeroSection.tsx
+++ my-project/src/page-components/HeroSection.tsx
@@ -7,13 +7,13 @@ export const HeroSection = ({ data }: { data: any }) => {
       <div className="hero container flex items-center xl:h-[800px]">
         <div className="mx-auto mb-96 mt-32 flex flex-col md:mb-[642px] md:max-w-[416px] xl:mb-0 xl:ml-0 xl:mt-0 xl:max-w-96 smOnly:p-2">
           <Markdown text={data.mainSection.heading} className="order-2" />
-          <div className="order-1 mb-8 flex flex-row justify-center gap-1 text-dark xl:justify-start">
+          <div className="order-1 mb-8 flex flex-row justify-center gap-1 text-bg_secondary xl:justify-start">
             <Paw className="h-12 w-12 flex-shrink-0 md:h-20 md:w-20" />
-            <p className="text-t50 md:text-t80">{data.mainSection.bigtext}</p>
+            <p className="text-t50  md:text-t80">{data.mainSection.bigtext}</p>
           </div>
           <Markdown
             text={data.mainSection.text}
-            className="order-3 !text-t16 md:!text-t24n"
+            className="order-3 !text-t16 !text-bg_secondary md:!text-t24n"
           />
         </div>
       </div>
