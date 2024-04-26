080f35902ac2628982b0affb0e6a8c6841fdc10e
smerch88 <maxarmyk@gmail.com>
2024-04-26 22:15:57 +0300

    new dog img

--- my-project/src/page-components/HeroSection.tsx
+++ my-project/src/page-components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Markdown } from "@/components/Markdown";
 import Image from "next/image";
-import HeroDog from "../../public/hero-dog.png";
+import HeroDog from "../../public/hero-dog.webp";
 import { AddToCartHeroBtn } from "@/components/AddToCartHeroBtn";
 
 export const HeroSection = ({ data }: { data: any }) => {
@@ -11,12 +11,13 @@ export const HeroSection = ({ data }: { data: any }) => {
           src={HeroDog}
           alt={"Emmy Dog"}
           className="absolute -bottom-16 -left-28 z-20 h-[360px] w-[298px] object-contain md:-bottom-20 md:-left-0 md:h-[420px] md:w-[338px] xl:-bottom-[110px] xl:left-0 xl:h-[584px] xl:w-[476px]"
+          sizes="50vw"
         />
         <Markdown
           text={data.mainSection.heading}
           className="mb-10 mt-28 md:mx-auto md:max-w-[400px] xl:mb-20 xl:mr-[280px] xl:mt-36 xl:max-w-[655px]"
         />
-        <p className="xl:text-t32n mb-4  font-sans text-t24n font-bold leading-7 tracking-[0.16px] xl:mr-[180px]">
+        <p className="mb-4 font-sans  text-t24n font-bold leading-7 tracking-[0.16px] xl:mr-[180px] xl:text-t32n">
           {data.mainSection.bigtext}
         </p>
         <AddToCartHeroBtn
