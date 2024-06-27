57a90c2df65d3e3cbdc5957f2f04e6670c8896b1
LongBeam <maxarmyk@gmail.com>
2024-06-27 18:21:55 +0300

    fix

--- my-project/next.config.mjs
+++ my-project/next.config.mjs
@@ -10,6 +10,22 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/en',
+        destination: '/ua',
+        permanent: true,
+      },
+      // Wildcard path matching
+      {
+        source: '/en/:slug',
+        destination: '/ua/:slug',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
